220053626e2bc3dffaf8ad1d70f9a28bfe07f81d
LawerenceLee <lucifitz.edward@gmail.com>
2018-01-03 20:28:23 -0800

    added detail and random views

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from random import randint
 
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Mineral
 
@@ -7,3 +8,14 @@ from .models import Mineral
 def index(request):
     minerals = Mineral.objects.all()
     return render(request, 'catalog/index.html', {"minerals": minerals})
+
+
+def detail(request, mineral_name):
+    mineral = get_object_or_404(Mineral, name=mineral_name)
+    return render(request, "catalog/detail.html", {"mineral": mineral})
+
+
+def random(request):
+    rand_num = randint(1, 873)
+    mineral = get_object_or_404(Mineral, pk=rand_num)
+    return redirect("/{}".format(mineral.name))
