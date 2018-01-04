5681898bbdc2639724f3e4808486f73ce9ef4ed9
LawerenceLee <lucifitz.edward@gmail.com>
2018-01-03 20:28:04 -0800

    added random and detail urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,5 +6,7 @@ from . import views
 # app_name needs to be set in order to server static files.
 app_name = 'catalog'
 urlpatterns = [
+    path('random', views.random, name="random"),
+    path('<str:mineral_name>', views.detail, name="detail"),
     path('', views.index, name="index"),
 ]
